74aec8939b81be8f550797b193df5bf2efb6af4e
ali sait bilgi <alisaitbilgi@gmail.com>
2016-11-29 09:06:25 +0200

    Update Request.js

--- javascript/Request.js
+++ javascript/Request.js
@@ -3,22 +3,48 @@
 const Request = {
   get: function (url) {
     return new Promise((res, rej) => {
-      let xhrObject = new XMLHttpRequest();
+      const xhrObject = new XMLHttpRequest();
       xhrObject.open("GET", url, true);
       xhrObject.send();
       xhrObject.onreadystatechange = function() {
-        if(xhrObject.status >= 200 && xhrObject.status < 300) {
+        const status = xhrObject.status;
+        if(status >= 200 && status < 300) {
           if(xhrObject.readyState === 4)
             res(xhrObject.response);
         }
-        else if(xhrObject.status >= 100 && xhrObject.status < 200)
-          rej(xhrObject.status + ": Continue");
-        else if(xhrObject.status >= 300 && xhrObject.status < 400)
-          rej(xhrObject.status + ": Temporary Redirect");
-        else if(xhrObject.status >= 400 && xhrObject.status < 500)
-          rej(xhrObject.status + ": Not Found");
-        else if(xhrObject.status >= 500 && xhrObject.status < 600)
-          rej(xhrObject.status + ": Internal Server Error");
+        else if(status >= 100 && status < 200)
+          rej(status + ": Continue");
+        else if(status >= 300 && status < 400)
+          rej(status + ": Temporary Redirect");
+        else if(status >= 400 && status < 500)
+          rej(status + ": Not Found");
+        else if(status >= 500 && status < 600)
+          rej(status + ": Internal Server Error");
+        else
+          rej("Process Failed");
+      };
+    });
+  },
+
+  post: function (url, data) {
+    return new Promise((res, rej) => {
+      const xhrObject = new XMLHttpRequest();
+      xhrObject.open("POST", url, true);
+      xhrObject.send(data);
+      xhrObject.onreadystatechange = function() {
+        const status = xhrObject.status;
+        if(xhrObject.status >= 200 && xhrObject.status < 300) {
+          if(xhrObject.readyState === 4)
+            res(data);
+        }
+        else if(status >= 100 && status < 200)
+          rej(status + ": Continue");
+        else if(status >= 300 && status < 400)
+          rej(status + ": Temporary Redirect");
+        else if(status >= 400 && status < 500)
+          rej(status + ": Not Found");
+        else if(status >= 500 && status < 600)
+          rej(status + ": Internal Server Error");
         else
           rej("Process Failed");
       };
