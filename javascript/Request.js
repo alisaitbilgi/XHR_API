8633ffdbaf54a64dfe7b5aac18627f04fd25f6b3
ali sait bilgi <alisaitbilgi@gmail.com>
2017-01-30 14:49:38 +0300

    Update Request.js

--- javascript/Request.js
+++ javascript/Request.js
@@ -1,32 +1,30 @@
 "use strict";
 
 
-        const handler = {
-            get: function (target, name) {
-                return name in target ? target[name] : function (url="", data) {
-                        let method = name.toUpperCase();
-                        return new Promise((res, rej) => {
-                            const xhr = new XMLHttpRequest();
-                            xhr.open(method, url, true);
-                            xhr.send(data);
-                            xhr.onload = function () {
-                                const status = xhr.status;
-                                if (status >= 200 && status < 300) {
-                                    res(xhr.response);
-                                } else if (status >= 400) {
-                                    rej(xhr.response);
-                                } else {
-                                    res(xhr.response);
-                                }
-                            };
-                            xhr.onerror = function (e) {
-                                rej(new Error(e));
-                            };
-                        });
+const handler = {
+    get: function (target, name) {
+        return name in target ? target[name] : function (url="", data) {
+                let method = name.toUpperCase();
+                return new Promise((res, rej) => {
+                    const xhr = new XMLHttpRequest();
+                    xhr.open(method, url, true);
+                    xhr.send(data);
+                    xhr.onload = function () {
+                        const status = xhr.status;
+                        if (status >= 200 && status < 300) {
+                            res(xhr.response);
+                        } else if (status >= 400) {
+                            rej(xhr.response);
+                        } else {
+                            res(xhr.response);
+                        }
                     };
-            }
-        };
+                    xhr.onerror = function (e) {
+                        rej(new Error(e));
+                    };
+                });
+            };
+    }
+};
 
-        const Request = new Proxy({}, handler);
-
-module.exports = Request;
+const Request = new Proxy({}, handler);
