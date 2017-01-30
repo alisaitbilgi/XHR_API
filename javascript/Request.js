2a37fcb6cca79b52e6c8ddb10e6268dc7234b6a3
ali sait bilgi <alisaitbilgi@gmail.com>
2017-01-30 14:48:15 +0300

    Update Request.js

--- javascript/Request.js
+++ javascript/Request.js
@@ -1,40 +1,32 @@
 "use strict";
 
-const handler = {
-  get: function (target, name) {
-    return name in target ? target[name] : function (url="", data) {
-      let method = name.toUpperCase();
-      return new Promise((res, rej) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url, true);
-        xhr.send(data);
-        xhr.onload = function () {
-          let responseObject = {
-            RequestURL: url,
-            RequestMethod: method,
-            StatusCode: xhr.status,
-            RequestPayload: data
-          };
 
-          if (data === null)
-            delete responseObject.RequestPayload;
-          
-          if (xhr.status >= 200 && xhr.status < 300)
-            res(responseObject);
-         
-          else if (xhr.status >= 400)
-            rej("404 Not Found");
-          
-          else
-            rej("Process Failed " + xhr.status);
+        const handler = {
+            get: function (target, name) {
+                return name in target ? target[name] : function (url="", data) {
+                        let method = name.toUpperCase();
+                        return new Promise((res, rej) => {
+                            const xhr = new XMLHttpRequest();
+                            xhr.open(method, url, true);
+                            xhr.send(data);
+                            xhr.onload = function () {
+                                const status = xhr.status;
+                                if (status >= 200 && status < 300) {
+                                    res(xhr.response);
+                                } else if (status >= 400) {
+                                    rej(xhr.response);
+                                } else {
+                                    res(xhr.response);
+                                }
+                            };
+                            xhr.onerror = function (e) {
+                                rej(new Error(e));
+                            };
+                        });
+                    };
+            }
         };
-        xhr.onerror = function (e) {
-          rej("There is a failure on the network level");
-        };
-      });
-    };
-  }
-};
-const Request = new Proxy({}, handler);
+
+        const Request = new Proxy({}, handler);
 
 module.exports = Request;
